e8624d5f3a33cbb5e559897c079e6dea68a6490f
Glitch (aipp-feature) <none>
2018-07-31 21:10:29 +0000

    🚓✈️ Checkpoint ./City_of_Austin_Public_Art_Collection.csv:710580/86346 ./index.html:710577/382:710580/18 ./script.js:710580/1786 ./style.css:710577/67

--- script.js
+++ script.js
@@ -1,5 +1,59 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+/***
 
-// prints "hi" in the browser's dev tools console
-console.log('hi');
+This example builds on skills to pull data from CSVs, which is covered in other exercises. 
+
+Examples of other kinds of bubble maps are available here: https://plot.ly/javascript/bubble-maps/
+
+Data from https://github.com/fivethirtyeight/data/tree/master/hate-crimes
+
+***/
+
+Plotly.d3.csv('/CoA_publicart.csv', function(err, rows){
+     
+      function unpack(rows, key) {
+          return rows.map(function(row) { return row[key]; });
+      }
+
+      var data = [{
+          type: 'scattermapbox',
+          lat: unpack(rows, 'Location Latitude'),
+          lon: unpack(rows, 'Location Longitude'),
+          text: unpack(rows,'FoundLocation'),
+      }];
+  
+
+
+      var layout = {
+       title: 'Pitbull Hot Spots',
+       font: {
+         color: '#222'
+       },
+        dragmode: 'zoom',
+        mapbox: { 
+          center: { // Sets the center of the map.
+            lat: 30.2672,
+            lon: -97.7431
+          },
+          zoom: 10, // Sets the zoom level.
+          style: 'light' // These are mapbox styles. Options include light, dark, satellite
+        },
+        margin: {
+          r: 20,
+          t: 40,
+          b: 20,
+          l: 20,
+          pad: 0
+        },
+        paper_bgcolor: '#ffffff',
+        plot_bgcolor: '#ffffff',
+        showlegend: false
+      };
+
+      Plotly.setPlotConfig({
+        mapboxAccessToken: 'pk.eyJ1IjoiYWxleGFuZHJhd2hpdGUiLCJhIjoiY2pmbHVxdmVhMHMyZjJ4bGk1eHdidDVjMCJ9.Bm90pkulu8KqsBfgDBO7lA' // Get your own Mapbox access token by registering at mapbox.com
+      });
+
+      Plotly.plot('viz', data, layout);
+});
+
+
