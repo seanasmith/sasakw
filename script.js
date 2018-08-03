bda6a108b70efd984ec4b7708f1986821f504af7
Glitch (aipp-feature) <none>
2018-08-03 21:04:38 +0000

    🎐🌚 Checkpoint ./index.html:710577/334 ./script.js:710577/894 ./.glitch-assets:710577/1212 ./data2.json:710577/175356

--- script.js
+++ script.js
@@ -80,64 +80,16 @@ $(document).ready(function(){
 
 // Filter list
 
-<!DOCTYPE html>
-<html>
 
-  <head>
-
-    <script src="data2.json"></script>
-
-    <style>
-    th, td {
-    text-align: right;
-    }
-    </style>
-
-  </head>
-
-  <body>
-
-    <h1>AFD Fire Incidents 2016</h1>
-
-    <form id="form1">
-      <select id="thedropdown">
-        <option value="0">Choose Month:</option>
-        <option value="Jan">Jan</option>
-        <option value="Feb">Feb</option>
-        <option value="Mar">Mar</option>
-        <option value="Apr">Apr</option>
-        <option value="May">May</option>
-        <option value="Jun">June</option>
-      </select>
-    </form>
-
-    <p>Total in Month: <span id="answer2"></span></p>
-
-    <table width="80%">
-
-      <thead>
-        <th>Incident</th>
-        <th>Month</th>
-        <th>Unit</th>
-        <th>Lat</th>
-        <th>Long</th>
-      </thead>
-
-      <tbody id="answer">
-      </tbody>
-
-    </table>
-
-    <script>
-    data = JSON.parse(data);
+    data2 = JSON.parse(data);
     document.getElementById("form1").onchange = function() {
     j=0;
 
     month = document.getElementById("thedropdown").value;
     document.getElementById("answer").innerHTML = " ";
-     for(i=0;i<data.length; i++) {
-      if(month == data[i].month) {
-     document.getElementById("answer").innerHTML += "<tr><td>" + data[i].incnum + "</td><td>" + data[i].month + "</td><td>" + data[i].unit + "</td><td>" + data[i].lat + "</td><td>" + data[i].long + "</td></tr>";
+     for(i=0;i<data2.length; i++) {
+      if(month == data2[i].month) {
+     document.getElementById("answer").innerHTML += "<tr><td>" + data[i].Zip + "</td><td>" + data[i].month + "</td><td>" + data[i].unit + "</td><td>" + data[i].lat + "</td><td>" + data[i].long + "</td></tr>";
      j=j+1;
      }
      }
