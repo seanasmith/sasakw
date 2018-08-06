e2940d6ef3e216d92d2410f3e788b91c14c1d968
Glitch (aipp-feature) <none>
2018-08-06 21:52:39 +0000

    📷🗼 Checkpoint ./script.js:710577/9445 ./index.html:710577/68 ./style.css:710577/139

--- script.js
+++ script.js
@@ -115,108 +115,80 @@ $(document).ready(function(){
   //78701
 
 google.charts.load('current', {packages: ['corechart', 'bar']});
-google.charts.setOnLoadCallback(drawBasic);
+google.charts.setOnLoadCallback(drawChart);
 
-function drawBasic() {
 
-      var data = google.visualization.arrayToDataTable([
-        ['Medium', 'Pieces of Public Art',],
-        ['Sculpture', 62],
-        ['Print', 4],
-        ['Painting', 14],
-        ['Mosaic', 2],
-        ['Landscape', 1],
-        ['Drawing', 1],
-        ['Mixed Media', 23]
+ function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Medium', '78701', '78702', '78719'],
+          ['Sculpture', 62, 43, 8],
+          ['Print', 4, 13, 7],
+          ['Painting', 14, 18, 6],
+          ['Mosaic', 2, 1, 0],
+          ['Landscape', 1, 3, 0],
+          ['Drawing', 1, 1, 0],
+          ['Mixed Media', 23, 5, 7]
+        ]);
+
+        var options = {
+          chart: {
+            title: 'Public Art Pieces by Medium',
+            subtitle: 'Top three Zipcodes',
+          },
+          bars: 'horizontal',// Required for Material Bar Charts.
+          height: 500,
+          width: 700,
+        };
+
+        var chart = new google.charts.Bar(document.getElementById('agg_bar'));
+
+        chart.draw(data, google.charts.Bar.convertOptions(options));
+      }
+    
+
+// Line chart
+
+
+
+google.charts.load('current', {packages: ['corechart', 'line']});
+google.charts.setOnLoadCallback(drawCurveTypes);
+
+function drawCurveTypes() {
+      var data2 = new google.visualization.DataTable();
+      data2.addColumn('number', 'X');
+      data2.addColumn('number', 'Dogs');
+      data2.addColumn('number', 'Cats');
+
+      data2.addRows([
+        [0, 0, 0],    [1, 10, 5],   [2, 23, 15],  [3, 17, 9],   [4, 18, 10],  [5, 9, 5],
+        [6, 11, 3],   [7, 27, 19],  [8, 33, 25],  [9, 40, 32],  [10, 32, 24], [11, 35, 27],
+        [12, 30, 22], [13, 40, 32], [14, 42, 34], [15, 47, 39], [16, 44, 36], [17, 48, 40],
+        [18, 52, 44], [19, 54, 46], [20, 42, 34], [21, 55, 47], [22, 56, 48], [23, 57, 49],
+        [24, 60, 52], [25, 50, 42], [26, 52, 44], [27, 51, 43], [28, 49, 41], [29, 53, 45],
+        [30, 55, 47], [31, 60, 52], [32, 61, 53], [33, 59, 51], [34, 62, 54], [35, 65, 57],
+        [36, 62, 54], [37, 58, 50], [38, 55, 47], [39, 61, 53], [40, 64, 56], [41, 65, 57],
+        [42, 63, 55], [43, 66, 58], [44, 67, 59], [45, 69, 61], [46, 69, 61], [47, 70, 62],
+        [48, 72, 64], [49, 68, 60], [50, 66, 58], [51, 65, 57], [52, 67, 59], [53, 70, 62],
+        [54, 71, 63], [55, 72, 64], [56, 73, 65], [57, 75, 67], [58, 70, 62], [59, 68, 60],
+        [60, 64, 56], [61, 60, 52], [62, 65, 57], [63, 67, 59], [64, 68, 60], [65, 69, 61],
+        [66, 70, 62], [67, 72, 64], [68, 75, 67], [69, 80, 72]
       ]);
 
-      var options = {
-        title: '78701',
-        chartArea: {width: '50%'},
-        height: 300,
+      var options2 = {
         hAxis: {
-          title: 'Public art pieces',
-          minValue: 0
+          title: 'Time'
         },
         vAxis: {
-          title: 'Medium'
+          title: 'Popularity'
+        },
+        series: {
+          1: {curveType: 'function'}
         }
       };
 
-      var chart = new google.visualization.BarChart(document.getElementById('78701_div'));
-
-      chart.draw(data, options);
-
-
-
-    //78702
-
-
-          var data2 = google.visualization.arrayToDataTable([
-            ['Medium', 'Pieces of Public Art',],
-            ['Sculpture', 43],
-            ['Print', 13],
-            ['Painting', 18],
-            ['Mosaic', 1],
-            ['Landscape', 3],
-            ['Drawing', 1],
-            ['Mixed Media', 5]
-          ]);
-
-          var options2 = {
-            title: '78702',
-            chartArea: {width: '50%'},
-            height: 300,
-            hAxis: {
-              title: 'Public art pieces',
-              minValue: 0
-            },
-            vAxis: {
-              title: 'Medium'
-            }
-          };
-
-          var chart2 = new google.visualization.PieChart(document.getElementById('78702_div'));
-
-          chart2.draw(data2, options2);
-
-
-    //78719
-
-    var data3 = google.visualization.arrayToDataTable([
-      ['Medium', 'Pieces of Public Art',],
-      ['Sculpture', 8],
-      ['Print', 7],
-      ['Painting', 6],
-      ['Mosaic', 0],
-      ['Landscape', 0],
-      ['Drawing', 0],
-      ['Mixed Media', 7]
-    ]);
-
-    var options3 = {
-      title: '78719',
-      chartArea: {width: '50%'},
-      height: 300,
-      hAxis: {
-        title: 'Public art pieces',
-        minValue: 0
-      },
-      vAxis: {
-        title: 'Medium'
-      }
-    };
-
-    var chart3 = new google.visualization.BarChart(document.getElementById('78719_div'));
-
-    chart3.draw(data3, options3);
-
-        }//close drawBasic
-
-
-
-
-
+      var chart = new google.visualization.LineChart(document.getElementById('agg_line'));
+      chart.draw(data2, options2);
+    }
 
 
 
